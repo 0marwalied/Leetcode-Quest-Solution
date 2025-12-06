41fe611e2404b75fd3319d405d08b191e11a545b
omar_walied <zaza03455@gmail.com>
2025-12-06 07:48:55 +0200

    Update solutions

---
+++ Data Structures and Algorithms/3- Association Slope/2- Hash/Q2. Copy List with Random Pointer.cpp
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+	Node* copyRandomList(Node* head) {
+		unordered_map<Node*, Node*>mp;
+		mp[NULL] = NULL;
+		Node *start = head;
+		while (start) {
+			mp[start] = new Node(start->val);
+			start = start->next;
+		}
+		start = head;
+		while (start) {
+			mp[start]->next = mp[start->next];
+			mp[start]->random = mp[start->random];
+			start = start->next;
+		}
+		return mp[head];
+
+	}
+};
